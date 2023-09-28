7fce7dff46af3a1a9f58894c120871e5669e0847
Jisha Joseph <jishaja14@gmail.com>
2023-09-28 16:14:29 +0530

    Home fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,67 +12,59 @@ import { ProductService } from '../services/product.service';
 export class HomeComponent implements OnInit{
 
 
-  categoriesArray:String[]=['smartphones','laptops','skincare']
+  //categoriesArray:String[]=['smartphones','laptops','skincare']
   product:IProductsList
   categoriesListArray:IProductsList[]=[]
-
+  product_smartphone:IProductsList
+  product_laptops:IProductsList
+  product_fragrances:IProductsList
+  
   constructor(private categoryService:CategoryService, private router:Router, private productService:ProductService){}
 
-  //outer ngfor to go through all categories
-  //inter for to display all products inside a category
-
   ngOnInit(): void {
-    this.getCategoryLists()
-    this.getCategory('smartphones')
     this.getCategory('laptops')
-    this.getCategory('fragrances')
-  }
-  // product_smartphone:IProductsList
-  // product_laptops:IProductsList
-  // product_fragrances:IProductsList
-
-  // displayProduct(){
-  //   console.log(this.product_fragrances,this.product_laptops,this.product_smartphone)
-  // }
-
-  getCategoryLists(){
-    this.categoryService.getCategoryList().subscribe(
-      (success:any)=>{
-        this.categoriesArray=success
-        //console.log(this.categoriesArray)
-      },
-      (error)=>{
-        console.log('home:',error)
-      }
-    )
+    this.getCategory('smartphones')
+    this.getCategory('fragrances') 
   }
 
-  getCategory(category:string='smartphones'){
-    this.categoryService.getCategory(category).subscribe((success:IProductsList)=>{
-      // if(category=='smartphones')
-      // {
-      //   this.product_smartphone=success
-      // }else if(category=='laptops'){
-      //   this.product_laptops=success
-      // }else if(category=='fragrances'){
-      //   this.product_fragrances=success
-      // }
-      this.categoriesListArray.push(success)
-      console.log('here',this.categoriesListArray)
-    })
-  }
 
-  message(mess:string){
-    console.log(mess)
-  }
 
   sendCategory(category_type:string){
     this.router.navigate(['/category/'+ category_type])
   }
 
   sendProduct(sendProduct:IProduct){
-    //console.log('send',sendProduct)
     this.productService.sendProduct(sendProduct)
   }
+  getCategory(category:string){
+    this.categoryService.getCategory(category).subscribe((success:IProductsList)=>{
+      if(category=='smartphones')
+      {
+        this.product_smartphone=success
+        console.log(category)
+      }else if(category=='laptops'){
+        this.product_laptops=success
+        console.log(category)
+      }else if(category=='fragrances'){
+        this.product_fragrances=success
+        console.log(category)
+      }
+      // this.categoriesListArray.push(success)
+    })
+  }
+
+  // getCategoryLists(){
+  //   this.categoryService.getCategoryList().subscribe(
+  //     (success:any)=>{
+  //       this.categoriesArray=success
+  //       //console.log(this.categoriesArray)
+  //     },
+  //     (error)=>{
+  //       console.log('home:',error)
+  //     }
+  //   )
+  // }
+
+  
 
 }
