9057e6f11a7beccace53e70d8820fce527fa2025
Jisha Joseph <jishaja14@gmail.com>
2025-10-19 00:47:34 +0530

    AngularV18 migration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductDisplayComponent } from './product-display/product-display.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProfileComponent } from './profile/profile.component';
@@ -21,34 +21,27 @@ import { WishlistComponent } from './wishlist/wishlist.component';
 import {CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { NgxSpinnerModule } from "ngx-spinner";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LayoutComponent,
-    NavbarComponent,
-    SidebarComponent,
-    LoginComponent,
-    CartComponent,
-    ProductDetailsComponent,
-    ProductDisplayComponent,
-    ProfileComponent,
-    SortPipe,
-    FilterPipe,
-    HomeComponent,
-    WishlistComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ToastrModule.forRoot(),
-    ReactiveFormsModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    NgxSpinnerModule
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LayoutComponent,
+        NavbarComponent,
+        SidebarComponent,
+        LoginComponent,
+        CartComponent,
+        ProductDetailsComponent,
+        ProductDisplayComponent,
+        ProfileComponent,
+        SortPipe,
+        FilterPipe,
+        HomeComponent,
+        WishlistComponent
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ToastrModule.forRoot(),
+        ReactiveFormsModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        NgxSpinnerModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
